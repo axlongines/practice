4ad80422c44b616a9a2219f33ebfef108ac1d1ed
AxelRuLo <developeraxrulo@gmail.com>
2022-07-26 20:11:56 -0500

    [IMP] open_academy: new menu item T#59020
    a new module was built, this model was about courses and had title and description and was about courses
    new data referenced to the module was added, this was about course model
    a view fo courses was build
    
    [IMP] open_academy: tree view, form view added T#59020
    
    the course view of tree was created
    the form editing and creation form was created
    the name of the model was also modified
    
    [IMP] open_academy: the .vcode was added to the gitingore T#59020
    
    [REF] open_academy: refactoring model courses and views T#59020
    
    the model was changed to match whit the convencions fo the oca, also the class of the module was changed to
    OpenAcademyCourse instead fo Course, the demo and views name's were changed too, and the .vscode folder was deleted
    
    [REM] open_academy: Controllers folder deleted and extra lines removed T#59020
    
    the extra lines in open_academy_course_views.xml was removed

---
+++ models/open_academy_course.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class OpenAcademyCourse(models.Model):
+    _name = "open.academy.course"
+    _description = "model of courses on open academy"
+
+    title = fields.Char(required=True, size=50)
+    description = fields.Text()
