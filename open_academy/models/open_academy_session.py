9494941ae34f9a3f2ac1080f22335efe11b0f777
AxelRuLo <developeraxrulo@gmail.com>
2022-08-02 13:40:45 -0500

    [IMP] open_academy: onchange and model constrains implementation T#59020
    The python constrains were added in order to check the instructor and the number of seats
    The sql constrains were added in order to make the title and the description diferents and the course name unique
    The view of graph, tree view whit extra color and calendar was added
    The view of kanban was added
    
    [IMP] open_academy: security implementation T#5920
    
    The security of the course and the session were implemented
    
    [IMP] open_academy: wizards implementation 59020
    
    [IMP] open_academy: relational fields and inherit T#59020
    
    The model of res.partner was inherit and some field were added
    The views of the model res.partner was modified addding the new fields
    The course and session model was modified in order to make it conected whit relational fields
    The acces fo the session was aded
    Corrections of the pr were made
    Changes in the strucutre of the tree view of the res.partner to make it more stylized
    Correction about the xpaht maded
    
    [REF] open_academy: refactor to make the linters mark succes
    
    [REF] open_academy: corrections of the comments in the PR
    
    [REF] open_academy: resolution of bad implementation on the computed exercise

--- open_academy/models/open_academy_session.py
+++ open_academy/models/open_academy_session.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class OpenAcademySession(models.Model):
@@ -6,9 +7,48 @@ class OpenAcademySession(models.Model):
     _description = "model of session on open academy"
 
     name = fields.Char(required=True, size=50)
-    duration = fields.Text()
-    start_date = fields.Datetime()
-    number_seats = fields.Integer()
+    duration = fields.Integer()
+    active = fields.Boolean(default=True)
+    start_date = fields.Datetime(default=fields.Date.context_today)
+    end_date = fields.Datetime()
+    number_seats = fields.Integer(required=True)
+    number_seats_taken = fields.Float(compute="_compute_number_taken_seats")
     instructor_id = fields.Many2one("res.partner", "instructor")
     course_id = fields.Many2one("open.academy.course", required=True)
     attendees_ids = fields.Many2many("res.partner", "session_res_partner_rel")
+    attendees_count = fields.Integer(compute="_compute_number_taken_seats", store=True)
+
+
+    @api.depends("number_seats", "attendees_ids")
+    def _compute_number_taken_seats(self):
+        for record in self:
+            number_attenndees = len(record.attendees_ids)
+            record.attendees_count = number_attenndees
+            if number_attenndees == 0 or record.number_seats == 0:
+                record.number_seats_taken = 0
+            else:
+                record.number_seats_taken = (number_attenndees / record.number_seats) * 100
+        
+
+    @api.onchange("number_seats", "attendees_ids")
+    def _onchange_number_seats(self):
+        if self.number_seats  < 0:
+            return {
+                "warning": {
+                    "title": "Incorrect 'seats' value.",
+                    "message": "The number of available seats may not be negative.",
+                }
+            }
+        if len(self.attendees_ids) > self.number_seats:
+            return {
+                "warning": {
+                    "title": "Too many attendees.",
+                    "message": "There are more attendees than seats.",
+                }
+            }
+
+    @api.constrains("instructor_id", "attendees_ids")
+    def _check_instructor_id(self):
+        for record in self.filtered('instructor_id'):
+            if record.instructor_id in record.attendees_ids:
+                raise ValidationError("The instructor can't be also an attendee.")
