006f70d879fa7408e0d80f8d8905432c97677fec
AxelRuLo <developeraxrulo@gmail.com>
2022-08-02 13:40:45 -0500

    [IMP] open_academy: onchange and model constrains implementation T#59020
    The python constrains were added in order to check the instructor and the number of seats
    The sql constrains were added in order to make the title and the description diferents and the course name unique
    The view of graph, tree view whit extra color and calendar was added
    The view of kanban was added
    
    [IMP] open_academy: security implementation T#5920
    
    The security of the course and the session were implemented
    
    [IMP] open_academy: wizards implementation 59020
    
    [IMP] open_academy: relational fields and inherit T#59020
    
    The model of res.partner was inherit and some field were added
    The views of the model res.partner was modified addding the new fields
    The course and session model was modified in order to make it conected whit relational fields
    The acces fo the session was aded
    Corrections of the pr were made
    Changes in the strucutre of the tree view of the res.partner to make it more stylized
    Correction about the xpaht maded
    
    [REF] open_academy: refactor to make the linters mark succes

---
+++ open_academy/wizard/open_academy_wizard.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+
+class OpenAcademyWizard(models.TransientModel):
+    _name = "open.academy.wizard"
+    _description = "wizard model on open academy"
+
+    def _default_sessions(self):
+        return self.env['open.academy.session'].browse(self._context.get('active_ids'))
+
+    sessions_id = fields.Many2many("open.academy.session", 'open_academy_wizard_open_academy_session_rel', 
+    default=_default_sessions)
+    attendees_ids = fields.Many2many('res.partner', 'open_academy_wizard_res_partner_rel')
+
+    def add_partners(self):
+        for session in self.sessions_id:
+            session.attendees_ids |= self.attendees_ids
